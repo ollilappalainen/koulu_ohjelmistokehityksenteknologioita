80f39ec214e59be9613123630d5a3eb7d6339817
Lappalainen Olli <a1503089@myy.haaga-helia.fi>
2017-08-22 10:13:24 +0300

    Week one exercises.

---
+++ week1-ex1/src/main/java/com/ollilappalainen/week1ex1/Week1Ex1Application.java
@@ -0,0 +1,12 @@
+package com.ollilappalainen.week1ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1Ex1Application.class, args);
+	}
+}
